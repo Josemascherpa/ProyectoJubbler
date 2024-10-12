d7e538d7072428b29efdf6990282999151f913ed
Josemascherpa <joseimascherpa@gmail.com>
2024-10-12 01:23:51 -0300

    Implements SQLite with model response

---
+++ app/src/main/java/com/mascherpa/proyectojubbler/database/DolarDatabase.java
@@ -0,0 +1,91 @@
+package com.mascherpa.proyectojubbler.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.mascherpa.proyectojubbler.model.Dolar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DolarDatabase extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "dolarDatabase.db";
+    private static final int DATABASE_VERSION = 2;
+
+    private static final String TABLE_NAME = "dolar_table";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_BUY = "buy";
+    private static final String COLUMN_SELL = "sell";
+    private static final String COLUMN_UPDATE_DATE = "updateDate";
+
+    public DolarDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_BUY + " TEXT, " +
+                COLUMN_SELL + " TEXT, " +
+                COLUMN_UPDATE_DATE + " TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertDolarIfNotExists(Dolar dolar) {
+        SQLiteDatabase db = this.getWritableDatabase();//para escribir bd
+        boolean inserted = false;
+
+        //mand oquery para ver si ya existe un registro con esa fecha
+        Cursor cursor = db.query(TABLE_NAME, new String[]{COLUMN_UPDATE_DATE},
+                COLUMN_UPDATE_DATE + " = ?", new String[]{dolar.getUpdateDate()},
+                null, null, null);
+
+        if (cursor.getCount() == 0) {
+            //si no existe creo uno nuevo,ya que es una fecha distinta
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_NAME, dolar.getName());
+            values.put(COLUMN_BUY, dolar.getBuy());
+            values.put(COLUMN_SELL, dolar.getSell());
+            values.put(COLUMN_UPDATE_DATE, dolar.getUpdateDate());
+
+            Log.d("Dolar","no existe registro, lo agrego");
+            db.insert(TABLE_NAME, null, values);
+            inserted = true;
+        }else{
+            Log.d("Dolar"," existe registro, no agrego");
+        }
+        cursor.close();
+        db.close();
+        return inserted;
+    }
+
+    public List<Dolar> getAllDolars() {
+        List<Dolar> dolars = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME, null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Dolar dolar = new Dolar(cursor.getString(0),cursor.getString(1),cursor.getString(2),cursor.getString(3));
+                dolars.add(dolar);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return dolars;
+    }
+
+
+}
--- app/src/main/java/com/mascherpa/proyectojubbler/model/Dolar.java
+++ app/src/main/java/com/mascherpa/proyectojubbler/model/Dolar.java
@@ -1,30 +1,38 @@
 package com.mascherpa.proyectojubbler.model;
 
+
+import com.google.gson.annotations.SerializedName;
+
 public class Dolar {
+    //uso del serilizedname para traerme solo los datos que necesito :)
+
+    @SerializedName("nombre")
     private String name;
+    @SerializedName("compra")
     private String buy;
+    @SerializedName("venta")
     private String sell;
+    @SerializedName("fechaActualizacion")
     private String updateDate;
 
-    public Dolar(String nameDolar,String buy,String sell,String updateDate){
-        this.name = nameDolar;
+    public Dolar(String name,String buy,String sell,String updateDate){
+        this.name=name;
         this.buy=buy;
-        this.sell = sell;
-        this.updateDate = updateDate;
+        this.sell=sell;
+        this.updateDate=updateDate;
     }
 
-    public String GetName() {
+    public String getName() {
         return name;
-    };
-    public String GetSell() {
-        return sell;
-    };
-    public String GetBuy() {
+    }
+    public String getBuy() {
         return buy;
-    };
-    public String GetUpdateDate() {
+    }
+    public String getSell() {
+        return sell;
+    }
+    public String getUpdateDate() {
         return updateDate;
-    };
-
+    }
 
 }
